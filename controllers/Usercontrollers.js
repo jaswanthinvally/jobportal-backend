3bd4a6d561e5fa3cc445533cb11aaefe39ec739a
jaswanthinvally <jaswanthvenkat001@gmail.com>
2024-08-22 07:00:55 +0530

    first commit :)

--- controllers/Usercontrollers.js
+++ controllers/Usercontrollers.js
@@ -1,8 +1,8 @@
 const UserModel = require('../models/Usermodels.js');
 
-exports.Usercontrollers = (req, res, next) => {
+exports.Createuser = (req, res, next) => {
     const { name, emailid, password } = req.body;
     UserModel.create({ name, emailid, password })
-        .then(() => res.json({ message: "success" }))
+        .then(() => res.json({message : "user created sucesfully", status: "success" }))
         .catch(err => res.status(500).json({ message: "failure", error: err.message }));
 };
--- routes/Userroutes.js
+++ routes/Userroutes.js
@@ -2,6 +2,6 @@ const express = require('express');
 const Usercontrollers = require('../controllers/Usercontrollers');
 const router = express.Router();
 
-router.route('/user').post(Usercontrollers.Usercontrollers);
+router.route('/user').post(Usercontrollers.Createuser);
 
 module.exports = router;
